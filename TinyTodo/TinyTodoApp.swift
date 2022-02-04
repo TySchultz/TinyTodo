c18ff90ab8220f7ef1e1d1d80f096542a473e33b
Tyler Schultz <tyler.schultz@onesky.com>
2022-02-03 22:19:52 -0500

    Initial Commit

---
+++ TinyTodo/TinyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTodoApp.swift
+//  TinyTodo
+//
+//  Created by Tyler Schultz on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TinyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
